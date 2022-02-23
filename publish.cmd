5ced11e6d70dd68488f7530adbcd256ccbb10c82
rakshita-reddy <70945363+rakshita-reddy@users.noreply.github.com>
2022-02-23 22:44:01 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnabotforcovid19-bot-8d18-Web-Deploy.pubxml -p:Password=8sxssll6xbz7t1hohP0stvXBgJtMNrFD4tSlv1SwCEKBfdPrha6z7LCk2wqw
+
